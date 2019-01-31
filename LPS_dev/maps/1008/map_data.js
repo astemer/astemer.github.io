0185bb4a56932991718927b3b9a6ce8d23471bb5
Stemmer <avner.stemmer@intel.com>
2019-01-31 23:23:46 +0200

    Add park lachish

---
+++ LPS_dev/maps/1008/map_data.js
@@ -0,0 +1,3 @@
+		var mapTitle = "פארק לכיש ליום הולדת מנחם";
+		var dataJson = 
+			{"ver":1,"ori_point_left_top_lat":"31.8307404","ori_point_left_top_long":"34.6313563","ori_point_right_bottom_lat":"31.8076940","ori_point_right_bottom_long":"34.6845713"};
