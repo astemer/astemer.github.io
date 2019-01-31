c87711c7c795f835359de26ee6734893cd1956dc
Stemmer <avner.stemmer@intel.com>
2019-02-01 00:00:01 +0200

    Add park Ad Halom

---
+++ LPS_dev/maps/1009/map_data.js
@@ -0,0 +1,3 @@
+		var mapTitle = "פארק עד הלום יום הולדת מנחם";
+		var dataJson = 
+			{"ver":1,"ori_point_left_top_lat":"31.7837951","ori_point_left_top_long":"34.6656164","ori_point_right_bottom_lat":"31.7772118","ori_point_right_bottom_long":"34.6750793"};
