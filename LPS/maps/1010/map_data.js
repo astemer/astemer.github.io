1ea1f6e76fbf5047ad8f1e752e79cb8bfe5aa8a2
Stemmer <avner.stemmer@intel.com>
2019-02-15 00:49:43 +0200

    Add map for Ukraine and Uman city

---
+++ LPS/maps/1010/map_data.js
@@ -0,0 +1,3 @@
+		var dataString = 			
+			"1.0;בעיקבות החסידות;heb;50.7397735;26.6528320;48.4492439;31.2670898";
+		document.getElementById("map_data").value = dataString;
---
+++ LPS_dev/maps/1011/map_data.js
@@ -0,0 +1,3 @@
+		var dataString = 			
+			"1.0;סביבת הציון;heb;48.7491064;30.2282089;48.7441787;30.2414911";
+		document.getElementById("map_data").value = dataString;
