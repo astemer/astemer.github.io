23e1c44acf80cf3972ec826ff906135aa3733c5d
Stemmer <avner.stemmer@intel.com>
2019-02-15 13:15:28 +0200

    Support Heb. But without infra for more langs.

--- LPS_dev/maps/1002/map_data.js
+++ LPS_dev/maps/1002/map_data.js
@@ -1,4 +1,4 @@
 		var dataString = 
-			"1.0;מפת ישראל;heb;33.6389522;33.2235350;29.0940005;36.8380370";
+			"1.0;מפת ישראל;eng;33.6389522;33.2235350;29.0940005;36.8380370";
 		document.getElementById("map_data").value = dataString;	
 			
