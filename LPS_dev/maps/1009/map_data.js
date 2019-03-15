797efb551efe90bd12d0bb15d0a690227b0eed75
Stemmer <avner.stemmer@intel.com>
2019-03-15 10:38:50 +0200

    Dynamic location calc for rotated map; Draw heading from north if availble

--- LPS_dev/maps/1009/map_data.js
+++ LPS_dev/maps/1009/map_data.js
@@ -1,3 +1,6 @@
+		
 		var dataString = 
-			"1.0;Intel - Jer5;heb;31.8035782;35.2106019;31.8030788;35.2108567";
+			// MAKE SURE EACH LITERAL DOES NOT CONTAIN THE DELIMITER!!
+			// Ver, Title, Lang, left-top-lat, left-top-long, right-bottom-lat, right-bottom-long			
+			"1.0;יער בן שמן - רשות הטבע והגנים;heb;31.9608116;34.9106026;31.9206858;35.0475884";
 		document.getElementById("map_data").value = dataString;
